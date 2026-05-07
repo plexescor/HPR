1cbca9110a1c3d80d54a1db7b488dc7ecbf5e926
plexescor <saraswal2010@gmail.com>
2026-05-07 17:21:43 +0530

    Add interpreter variables

--- include/HPRInterpreter.hpp
+++ include/HPRInterpreter.hpp
@@ -1,34 +1,38 @@
-// // HPR.hpp
-// #pragma once
+// HPR.hpp
+#pragma once
 
-// #include "appState.hpp"
-// #include "aliasManager.hpp"
-// #include "uiModelManager.hpp"
+#include "appState.hpp"
+#include "aliasManager.hpp"
+#include "uiModelManager.hpp"
 
-// //Slint stuff
-// #include <slint-interpreter.h>
+//Slint stuff
+#include <slint-interpreter.h>
 
-// #include <thread>
-// #include <atomic>
-// #include <optional>
+#include <thread>
+#include <atomic>
+#include <optional>
 
-// class HPRInterpreter {
-//     public:
-//         HPRInterpreter();
-//         ~HPRInterpreter();
-//         void show();
-//         void hide();
-//         void run(); // blocking call i believe
-//         void quit();
+class HPRInterpreter {
+    public:
+        HPRInterpreter();
+        ~HPRInterpreter();
+        void show();
+        void hide();
+        void run(); // blocking call i believe
+        void quit();
 
-//     private:
-//         void trackingLoop(); // runs on separate thread so that it polls shit continously (correct spelling?)
+    private:
+        void trackingLoop(); // runs on separate thread so that it polls shit continously (correct spelling?)
 
-//     private:
-//         slint::ComponentHandle<MainWindow> ui;
-//         std::atomic<bool> running{true};
-//         std::thread tracker;
+    private:
+        //Interpreter stuff
+        slint::interpreter::ComponentCompiler compiler;
+        std::optional<slint::interpreter::ComponentDefinition> definition;
+        slint::ComponentHandle<slint::interpreter::ComponentInstance> instance;
 
-//         AliasManager aliasManager;
-//         UiModelManager modelManager;
-// };
+        std::atomic<bool> running{true};
+        std::thread tracker;
+
+        AliasManager aliasManager;
+        UiModelManager modelManager;
+};
