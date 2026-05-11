de94f17f02292e54925c0d7c89729375586cf460
plexescor <saraswal2010@gmail.com>
2026-05-11 19:37:38 +0530

    fix: update tracking loop to respect running state and change Ko-fi button icon

--- src/ui-cpp/HPR.cpp
+++ src/ui-cpp/HPR.cpp
@@ -170,7 +170,7 @@ void HPR::trackingLoop()
         }
 
         //Chunked sleep
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5 && running; i++)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
